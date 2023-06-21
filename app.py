5503fac0014df7f21600c05955cfdb092d986a10
Safiullah Rahu <safiullahrahu1555@gmail.com>
2023-06-21 23:01:33 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Importing the required modules
 import os 
 import streamlit as st
 from streamlit_chat import message
@@ -9,68 +10,35 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA, ConversationalRetrievalChain
 from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.conversational_retrieval.prompts import CONDENSE_QUESTION_PROMPT
 from langchain.chains.question_answering import load_qa_chain
 from langchain.vectorstores import Pinecone
 import pinecone 
 
-
+# Setting up logging configuration
 logger = logging.getLogger("AI_Chatbot")
+
+# Setting up Streamlit page configuration
 st.set_page_config(
     page_title="AI Chatbot", layout="wide", initial_sidebar_state="expanded"
 )
 
+# Getting the OpenAI API key from Streamlit Secrets
+openai_api_key = st.secrets.secrets.OPENAI_API_KEY
+os.environ["OPENAI_API_KEY"] = openai_api_key
 
-def authenticate(
-    openai_api_key: str, PINECONE_API_KEY: str, PINECONE_ENV: str
-) -> None:
-    # Validate all credentials are set and correct
-    # Check for env variables to enable local dev and deployments with shared credentials
-    openai_api_key = (
-        openai_api_key
-        or os.environ.get("OPENAI_API_KEY")
-        or st.secrets.get("OPENAI_API_KEY")
-    )
-    os.environ["OPENAI_API_KEY"] = openai_api_key
-    PINECONE_API_KEY = (
-        PINECONE_API_KEY
-        or os.environ.get("PINECONE_API_KEY")
-        or st.secrets.get("PINECONE_API_KEY")
-    )
-    os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
-    PINECONE_ENV = (
-        PINECONE_ENV
-        or os.environ.get("PINECONE_ENV")
-        or st.secrets.get("PINECONE_ENV")
-    )
-    os.environ["PINECONE_ENV"] = PINECONE_ENV
-    if not (openai_api_key and PINECONE_API_KEY and PINECONE_ENV):
-        st.session_state["auth_ok"] = False
-        st.error("Credentials neither set nor stored")#, icon=PAGE_ICON)
-        return
-    try:
-        # Try to access openai and deeplake
-        with st.spinner("Authentifying..."):
-            openai.api_key = openai_api_key
-    except Exception as e:
-        logger.error(f"Authentication failed with {e}")
-        st.session_state["auth_ok"] = False
-        st.error("Authentication failed")#, icon=PAGE_ICON)
-        return
-    # store credentials in the session state
-    st.session_state["auth_ok"] = True
-    st.session_state["openai_api_key"] = openai_api_key
-    st.session_state["PINECONE_API_KEY"] = PINECONE_API_KEY
-    st.session_state["PINECONE_ENV"] = PINECONE_ENV
-    logger.info("Authentification successful!")
-    st.sidebar.success("Authentification successful!")
-    # return True
-
-# # Page options and header
-# st.set_option("client.showErrorDetails", True)
+# Getting the Pinecone API key and environment from Streamlit Secrets
+PINECONE_API_KEY = st.secrets.secrets.PINECONE_API_KEY
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+PINECONE_ENV = st.secrets.secrets.PINECONE_ENV
+os.environ["PINECONE_ENV"] = PINECONE_ENV
 
 
+# Defining the main function
 def main():
+    # Displaying the heading of the chatbot
     st.markdown(
         """
         <div style='text-align: center;'>
@@ -79,6 +47,7 @@ def main():
         """,
         unsafe_allow_html=True,
     )
+    # Displaying the description of the chatbot
     st.markdown(
         """
         <div style='text-align: center;'>
@@ -124,42 +93,76 @@ def admin():
 
 
 def chat():
+    # Initialize Pinecone with API key and environment
+    pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENV)
+    
+    # Set the model name and Pinecone index name
+    model_name = "gpt-3.5-turbo" 
     pinecone_index = "aichat"
+
+    # Set the text field for embeddings
     text_field = "text"
+
+    # Create OpenAI embeddings
     embeddings = OpenAIEmbeddings(model = 'text-embedding-ada-002')
+
+    # load a Pinecone index
     index = pinecone.Index(pinecone_index)
     db = Pinecone(index, embeddings.embed_query, text_field)
     retriever = db.as_retriever()
-    llm = ChatOpenAI(model='gpt-3.5-turbo') # 'gpt-3.5-turbo',
+
+    # Enable GPT-4 model selection
+    mod = st.sidebar.checkbox('Access GPT-4')
+    if mod:
+        pas = st.sidebar.text_input("Write access code", type="password")
+        if pas == "ongpt":
+            MODEL_OPTIONS = ["gpt-3.5-turbo", "gpt-4"]
+            model_name = st.sidebar.selectbox(label="Select Model", options=MODEL_OPTIONS)
+
+    
+    # Create ChatOpenAI model and RetrievalQA
+    llm = ChatOpenAI(model=model_name) # 'gpt-3.5-turbo',
     qa = RetrievalQA.from_chain_type(llm=llm,
                                      chain_type="stuff", 
                                      retriever=retriever, 
                                      verbose=True)
-
+    
+    # Define the prompt form
     def prompt_form():
             """
             Displays the prompt form
             """
             with st.form(key="my_form", clear_on_submit=True):
+                # User input
                 user_input = st.text_area(
                     "Query:",
                     placeholder="Ask me your queries...",
                     key="input_",
                     label_visibility="collapsed",
                 )
+
+                # Submit button
                 submit_button = st.form_submit_button(label="Send")
                 
+                # Check if the form is ready
                 is_ready = submit_button and user_input
             return is_ready, user_input
+    
+    # Define the conversational chat function
     def conversational_chat(query):
         
         # chain_input = {"question": query}#, "chat_history": st.session_state["history"]}
         # result = chain(chain_input)
+        # llm = ChatOpenAI(model=model_name)
+        # docs = db.similarity_search(query)
+        # qa = load_qa_chain(llm=llm, chain_type="stuff")
+        # Run the query through the RetrievalQA model
         result = qa.run(query) #chain({"question": query, "chat_history": st.session_state['history']})
         st.session_state['history'].append((query, result))#["answer"]))
     
-        return result#["answer"]
+        return result   #["answer"]
     
+    # Initialize session state variables
     if 'history' not in st.session_state:
         st.session_state['history'] = []
 
@@ -168,66 +171,49 @@ def chat():
 
     if 'past' not in st.session_state:
         st.session_state['past'] = ["Hey ! 👋"]
-        
+    
+    # Reset chat button
     res = st.button("Reset Chat")
+
+    # Prompt form input and chat processing
     is_ready, user_input = prompt_form()
     if is_ready:
         output = conversational_chat(user_input)
-        
         st.session_state['past'].append(user_input)
         st.session_state['generated'].append(output)
 
+    # Display chat messages
     if st.session_state['generated']:
         for i in range(len(st.session_state['generated'])-1, -1, -1):
-            message(st.session_state["generated"][i], key=str(i))
-            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
+            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile",)
     
+    # Reset chat session state
     if res:
         st.session_state['generated'] = []
         st.session_state['past'] = []
 
-with st.sidebar:
-    st.title("Authenticating Credentials")
-    with st.form("authentication"):
-        openai_api_key = st.text_input(
-            "OpenAI API Key",
-            type="password",
-            #help=OPENAI_HELP,
-            placeholder="This field is mandatory",
-        )
-        PINECONE_API_KEY = st.text_input(
-            "Pinecone API Key",
-            type="password",
-            #help=ACTIVELOOP_HELP,
-            placeholder="This field is mandatory",
-        )
-        PINECONE_ENV = st.text_input(
-            "Pinecone Env",
-            type="password",
-            #help=ACTIVELOOP_HELP,
-            placeholder="This field is mandatory",
-        )
-        submitted = st.form_submit_button("Submit")
-        if submitted:
-            authenticate(openai_api_key, PINECONE_API_KEY, PINECONE_ENV)
 
-os.environ["OPENAI_API_KEY"] = openai_api_key
-
+# List of available functions: Home, Chatbot, Admin
 functions = [
         "Home",
-        "AI Chatbot",
+        "Chatbot",
         "Admin",
     ]
 
-
-
+# Display a select box in the sidebar to choose the desired function
 selected_function = st.sidebar.selectbox("Select Option", functions)
+
+# Call the main() function if "Home" is selected
 if selected_function == "Home":
     main()
-elif selected_function == "AI Chatbot":
+# Call the chat() function if "Chatbot" is selected
+elif selected_function == "Chatbot":
     chat()
 elif selected_function == "Admin":
+    # Display a text input box in the sidebar to enter the password
     passw = st.sidebar.text_input("Enter your password: ", type="password")
+    # Call the admin() function if the correct password is entered
     if passw == "ai4chat":
         admin()
     
